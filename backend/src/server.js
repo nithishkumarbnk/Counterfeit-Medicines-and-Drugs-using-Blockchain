db0d4dac83d9e5376adcd32121e64ddb050bc7a4
nithishkumarbnk <nithishkumar.bhu07@gmail.com>
2025-06-30 18:20:25 +0530

    frontend error handeld

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const { Web3 } = require("web3");
+const Web3 = require("web3");
 const { HttpProvider, WebsocketProvider } = require("web3-providers");
 const path = require("path");
 const fs = require("fs");
